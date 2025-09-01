d28d1d70e30a4c2063429a77da89f10ad7e58125
MiliteSGT <milite.concursos@gmail.com>
2025-09-01 03:16:09 -0300

    Adding Classes

---
+++ code/menu.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class Menu:
+    def __init__(self):
+        self.window = None
+        self.Attribute1 = None
+
+    def run(self, ):
+        pass
+
+    def name(self, ):
+        pass
+
+    def Operation1(self, ):
+        pass
+
+    def name(self, ):
+        pass
+
+    def name(self, ):
+        pass
