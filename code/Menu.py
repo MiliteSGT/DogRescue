563f32dc84d89c886c0ba606bd0c29b3fe2dadd8
MiliteSGT <milite.concursos@gmail.com>
2025-09-01 10:12:52 -0300

    asset new

--- code/Menu.py
+++ code/Menu.py
@@ -1,22 +1,16 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import pygame.image
+
 
 class Menu:
     def __init__(self, window):
         self.window = window
-        self.Attribute1 = None
+        self.surf = pygame.image.load('./asset/MenuBg.png')
+        self.rect = self.surf.get_rect(left=0, top=0)
 
     def run(self, ):
+        self.window.blit(source=self.surf, dest=self.rect)
+        pygame.display.flip()
         pass
 
-    def name(self, ):
-        pass
-
-    def Operation1(self, ):
-        pass
-
-    def name(self, ):
-        pass
-
-    def name(self, ):
-        pass
